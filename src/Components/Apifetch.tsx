c7d885697489b5a32213492543b698711d54cb7f
prajwal-web <prajuu2000@gmail.com>
2025-02-09 08:56:43 +0530

    first commit

--- src/Components/Apifetch.tsx
+++ src/Components/Apifetch.tsx
@@ -1,7 +1,7 @@
 import getImageApi from "./getImageApi";
 import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import { Grid2 } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 
 const Apifetch = () => {
   const [images, setImages] = useState([]);
@@ -13,17 +13,19 @@ const Apifetch = () => {
   }, []);
   return (
     <>
-      <Grid2 container spacing={2} sx={{ m: 5 }}>
+      <Grid2 container spacing={10} sx={{ m: 5, marginTop: 10 }}>
         {images.map((image: any) => (
-          <Grid2 size={{ xs: 12, sm: 8, md: 4 }} key={image.id}>
+          <Grid2 size={{ xs: 12, sm: 6, md: 6 }} key={image.id}>
             <Box
               component="img"
               sx={{
                 width: "100%",
+                height: "100%",
               }}
               alt={image.title}
               src={image.download_url}
             />
+            <Typography textAlign="center">{image.author}</Typography>
           </Grid2>
         ))}
       </Grid2>
